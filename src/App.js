21a6c92b07a89c5f17aa9db17f5a1559ad6849b3
Emilio Calvo <emiliojosecalvo@gmail.com>
2022-02-15 15:49:35 +0000

    reducer example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useReducer } from 'react';
 import './App.css';
 
+function countReducer(state, action) {
+  switch (action.type) {
+    case ('ADD'):
+      return { count: state.count + action.amount }
+    case ('REMOVE'):
+      return { count: state.count - action.amount }
+    case ('RESET'):
+      return { count: 0 }
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(countReducer, { count: 0 })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count: {state.count}</h1>
+      <button onClick={() => dispatch({ type: 'ADD', amount: 1 })}>Add 1</button>
+      <button onClick={() => dispatch({ type: 'ADD', amount: 5 })}>Add 5</button>
+      <button onClick={() => dispatch({ type: 'REMOVE', amount: 1 })}>Remove 1</button>
+      <button onClick={() => dispatch({ type: 'REMOVE', amount: 5 })}>Remove 5</button>
+      <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
     </div>
   );
 }
